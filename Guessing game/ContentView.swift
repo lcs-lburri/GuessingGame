423eff89dc6cecc5f5da55c90831495620b61af7
Luke Burri <LBUrri@lcs.on.ca>
2020-11-17 15:32:57 -0500

    got the user's guess to almost be checked

--- Guessing game/ContentView.swift
+++ Guessing game/ContentView.swift
@@ -12,6 +12,12 @@ struct ContentView: View {
     // the guess made by the user
     @State private var theUserGuess = "50"
     
+    //the number that the user should guess
+    let taget = Int.random(in: 1...100)
+    
+    //feedback to the user
+    @State private var feedback = ""
+    
     var body: some View {
         
         NavigationView {
@@ -22,6 +28,7 @@ struct ContentView: View {
                 
                 Text("I'm thinking of a number between 1 and 100. Guess what it is!")
                     .font(.title)
+                    .padding(.horizontal, 20.0)
                 
                 TextField("ENter your guess here",
                           text: $theUserGuess)
@@ -30,6 +37,10 @@ struct ContentView: View {
                 }
                 
                 Text("You guessed \(theUserGuess).")
+                    .font(.title)
+                    .padding(.horizontal, 15.0)
+                
+                Text("\(feedback)")
                     .font(.title)
                 
                 Spacer()
@@ -40,6 +51,17 @@ struct ContentView: View {
         }
     }
     
+    func checkGuess() {
+        
+        //see if the user gave us an integer in the expected range
+        guard let givenInterager = Int(theUserGuess) else {
+            feedback = "please provide an interger between 1 and 100."
+            return
+        }
+        guard givenInterager > 0, givenInterager < 101 else {
+            feedback = "please provide an interger between 1 and 100."}
+        // is the guess correct
+    }
 }
 
 
