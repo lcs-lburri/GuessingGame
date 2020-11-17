e1e39fbd3fad144610c76d2488f9081e639a428d
Luke Burri <LBUrri@lcs.on.ca>
2020-11-17 15:21:12 -0500

    formatting is a bit better

--- Guessing game/ContentView.swift
+++ Guessing game/ContentView.swift
@@ -10,40 +10,43 @@ import SwiftUI
 struct ContentView: View {
     
     // the guess made by the user
-    @State private var theUserGuess = ""
+    @State private var theUserGuess = "50"
     
     var body: some View {
         
         NavigationView {
             
-        
-        VStack {
-            Text("Guessing Game")
             
-            Text("I'm thinking of a number between 1 and 100. Guess what it is!")
-            
-            TextField("ENter your guess here",
-                      text: $theUserGuess)
-            Button("Submit Guess") {
-                //cheack the users guess
+            VStack {
+                
+                
+                Text("I'm thinking of a number between 1 and 100. Guess what it is!")
+                    .font(.title)
+                
+                TextField("ENter your guess here",
+                          text: $theUserGuess)
+                Button("Submit Guess") {
+                    //cheack the users guess
+                }
+                
+                Text("You guessed \(theUserGuess).")
+                    .font(.title)
+                
+                Spacer()
             }
+            .navigationTitle("Guessing Game")
             
-            Text("You guessed \(theUserGuess).")
-        }
-        .navigationTitle("Guessing Game")
-        
-        }
-        
-        struct ContentView_Previews: PreviewProvider {
-            static var previews: some View {
-                ContentView()
-            }
+            
         }
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+        
+        ContentView()
     }
+    
 }
