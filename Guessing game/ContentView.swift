35aa156ddcfb8f2cbbae36f95254012b484bfcfa
Luke Burri <LBUrri@lcs.on.ca>
2020-11-17 14:41:54 -0500

    Initial Commit

---
+++ Guessing game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guessing game
+//
+//  Created by Burri, Luke on 2020-11-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guessing game/Guessing_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_gameApp.swift
+//  Guessing game
+//
+//  Created by Burri, Luke on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
